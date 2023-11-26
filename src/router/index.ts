c1960940f2aad5cc58ead13bd790230347c67ddb
Martin Matejovsky <matejovsky.ma@volny.cz>
2023-11-26 19:50:39 +0100

    cleaning and configuring default homepage templates

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,13 +8,13 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
+    path: "/lightsOut",
+    name: "gameLightsOut",
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (lightsOut.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "lightsOut" */ "../views/LightsOutGame.vue"),
   },
 ];
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>Game in typescript</h1>
+    <button @click="openLightsOut">Game Lights Out</button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "HomeView",
-  components: {
-    HelloWorld,
+  methods: {
+    openLightsOut() {
+      this.$router.push("lightsOut");
+    },
   },
 });
 </script>
