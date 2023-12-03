e1f9f897696bb409d57b18e59f30ec86190313f1
Martin Matejovsky <matejovsky.ma@volny.cz>
2023-12-03 19:53:38 +0100

    new Util class Range

---
+++ src/utils/classes.ts
@@ -0,0 +1,17 @@
+export class Range {
+  min: number;
+  max: number;
+
+  public get length(): number {
+    return this.max - this.min;
+  }
+
+  generateRandom(): number {
+    return Math.floor(Math.random() * (this.max - this.min) + this.min);
+  }
+
+  constructor(min: number, max: number) {
+    this.min = min;
+    this.max = max;
+  }
+}
