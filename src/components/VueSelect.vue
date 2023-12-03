685d7f9abc29269a378c6cc2dff504a05eab95af
Martin Matejovsky <matejovsky.ma@volny.cz>
2023-12-03 19:23:26 +0100

    new component VueSelect

---
+++ src/components/VueSelect.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="c-vue-select">
+    <label :for="`vue-select-${name}`"></label>
+    <select :name="`vue-select-${name}`">
+      <option
+        v-for="option in options"
+        :key="option.value"
+        :value="option.value"
+      >
+        {{ option.text }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+// ========= interface
+interface Option {
+  value: number;
+  text: string;
+}
+
+// ========= export
+export default defineComponent({
+  name: "VueSelect",
+  props: {
+    name: {
+      required: true,
+      type: String,
+    },
+    options: {
+      required: true,
+      type: Object as PropType<Option[]>,
+    },
+  },
+  data: function () {
+    return {
+      selectedValue: null as string | null,
+    };
+  },
+  computed: {
+    selectId(): string {
+      return `vue-select-${this.name}`;
+    },
+  },
+  watch: {
+    selectedValue: function (newValue) {
+      this.$emit("select", newValue);
+    },
+  },
+  mounted: function () {
+    const select = document.querySelector(
+      `select[name="${this.selectId}"]`
+    ) as HTMLSelectElement;
+    select.addEventListener("change", () => {
+      this.selectedValue = select.value;
+    });
+  },
+});
+</script>
