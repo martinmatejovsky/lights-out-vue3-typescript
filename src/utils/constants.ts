6b47813953e7c9f4988f3d68bdab567c4317b996
Martin Matejovsky <matejovsky.ma@volny.cz>
2023-12-05 19:08:33 +0100

    utils page for project constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+export const colorsLightsOut = [
+  { value: "#FFA500", text: "Orange" },
+  { value: "#FFFF00", text: "Yellow" },
+  { value: "#008000", text: "Green" },
+  { value: "#0000FF", text: "Blue" },
+  { value: "#800080", text: "Purple" },
+];
