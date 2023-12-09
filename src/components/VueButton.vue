b62eecbefa9ae8393ccbdf7e201e88349974a8d6
Martin Matejovsky <matejovsky.ma@volny.cz>
2023-12-09 12:44:25 +0100

    new component Vue Button

---
+++ src/components/VueButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="c-vue-button">
+    <button :class="['vue-button-button', componentClasses]">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "VueButton",
+  props: {
+    type: {
+      required: false,
+      type: String,
+      default: "button",
+    },
+  },
+  computed: {
+    componentClasses(): string {
+      return "is-" + this.type;
+    },
+  },
+});
+</script>
