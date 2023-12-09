044d0414d9effcfb1521522413a29078abfa4ce3
Martin Matejovsky <matejovsky.ma@volny.cz>
2023-12-09 12:53:17 +0100

    refactor of component LughtsOut and page LightsOutGame

--- src/components/LightsOut.vue
+++ src/components/LightsOut.vue
@@ -1,80 +1,16 @@
 <template>
-  <div class="c-lightsOut">
-    <p>Select game parameters</p>
-    <div class="lights-out-settings">
-      <VueSelect name="grid" :options="gridOptions" @select="updateGrid"
-        >Size of a field</VueSelect
-      >
-      <VueSelect name="colors" :options="colorsOptions" @select="updateColors"
-        >Number of colors</VueSelect
-      >
-      <button
-        class="lights-out-button-start"
-        :disabled="!colors || !grid"
-        @click="startGame"
-      >
-        Start game
-      </button>
-    </div>
-    <p>{{ grid }}</p>
-    <p>{{ colors }}</p>
-  </div>
+  <div class="c-lightsOut"></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import VueSelect from "@/components/VueSelect.vue";
-import { Range } from "@/utils/classes";
-import { SelectOptions } from "@/utils/interfaces";
-import { gameStates } from "@/utils/constants";
 
 export default defineComponent({
   name: "LightsOut",
-  components: { VueSelect },
   props: {},
   data: function () {
-    return {
-      grid: null as number | null,
-      colors: null as number | null,
-      gridOptions: [] as SelectOptions[],
-      colorsOptions: [] as SelectOptions[],
-      state: gameStates.new,
-    };
+    return {};
   },
-  created() {
-    this.generateOptionsProps();
-    this.generateColorsOptions();
-  },
-  methods: {
-    startGame(): void {
-      this.state = gameStates.inProgress;
-    },
-    updateGrid(value: string): void {
-      this.grid = parseInt(value);
-    },
-    updateColors(value: string): void {
-      this.colors = parseInt(value);
-    },
-    generateOptionsProps(): void {
-      const gridOptionsRange = new Range(3, 10);
-      for (
-        let i: number = gridOptionsRange.min;
-        i <= gridOptionsRange.max;
-        i++
-      ) {
-        this.gridOptions.push({ value: i, text: i.toString() });
-      }
-    },
-    generateColorsOptions(): void {
-      const colorOptionsRange = new Range(2, 4);
-      for (
-        let i: number = colorOptionsRange.min;
-        i <= colorOptionsRange.max;
-        i++
-      ) {
-        this.colorsOptions.push({ value: i, text: i.toString() });
-      }
-    },
-  },
+  methods: {},
 });
 </script>
--- src/views/LightsOutGameView.vue
+++ src/views/LightsOutGameView.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="home">
     <h1>Lights out</h1>
+    <h2>Select game parameters</h2>
+
+    <div class="lights-out-settings">
+      <VueSelect name="grid" :options="gridOptions" @select="updateGrid"
+        >Size of a field</VueSelect
+      >
+      <VueSelect name="colors" :options="colorsOptions" @select="updateColors"
+        >Number of colors</VueSelect
+      >
+      <VueButton type="submit" :disabled="!colors || !grid" @click="startGame"
+        >Start game</VueButton
+      >
+    </div>
+    <p>{{ grid }}</p>
+    <p>{{ colors }}</p>
     <LightsOut />
   </div>
 </template>
@@ -8,11 +23,62 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import LightsOut from "../components/LightsOut.vue";
+import VueButton from "@/components/VueButton.vue";
+import VueSelect from "@/components/VueSelect.vue";
+import { SelectOptions } from "@/utils/interfaces";
+import { gameStates } from "@/utils/constants";
+import { Range } from "@/utils/classes";
 
 export default defineComponent({
   name: "LightsOutView",
   components: {
+    VueSelect,
+    VueButton,
     LightsOut,
+  },
+  data() {
+    return {
+      grid: null as number | null,
+      colors: null as number | null,
+      gridOptions: [] as SelectOptions[],
+      colorsOptions: [] as SelectOptions[],
+      state: gameStates.new,
+    };
+  },
+  created() {
+    this.generateOptionsProps();
+    this.generateColorsOptions();
+  },
+  methods: {
+    generateOptionsProps(): void {
+      const gridOptionsRange = new Range(3, 10);
+      for (
+        let i: number = gridOptionsRange.min;
+        i <= gridOptionsRange.max;
+        i++
+      ) {
+        this.gridOptions.push({ value: i, text: i.toString() });
+      }
+    },
+    generateColorsOptions(): void {
+      const colorOptionsRange = new Range(2, 4);
+      for (
+        let i: number = colorOptionsRange.min;
+        i <= colorOptionsRange.max;
+        i++
+      ) {
+        this.colorsOptions.push({ value: i, text: i.toString() });
+      }
+    },
+    updateGrid(value: number): void {
+      this.grid = value;
+    },
+    updateColors(value: number): void {
+      this.colors = value;
+    },
+    startGame(): void {
+      this.state = gameStates.inProgress;
+    },
   },
 });
 </script>
