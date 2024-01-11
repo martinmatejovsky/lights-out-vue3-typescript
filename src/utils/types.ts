6b4e59f33153d40697726728fc0c4988a4afdded
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-01-11 10:56:58 +0100

    Typescript types definition refactor

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,15 +1,17 @@
 import { GameStates } from "./constants";
 
-export type SelectOptions = {
+type SelectOptions = {
   value: string | number | null;
   text: string;
 };
 
-export type CellCoordinates = {
+type CellCoordinates = {
   row: number;
   column: number;
 };
 
-export type SwitchDirection = "forward" | "backward";
+type SwitchDirection = "forward" | "backward";
 
-export type GameState = keyof typeof GameStates;
+type GameState = keyof typeof GameStates;
+
+export { SelectOptions, CellCoordinates, SwitchDirection, GameState };
